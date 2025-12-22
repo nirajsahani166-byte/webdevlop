abec86f1c8107761727c04e31b40132dc78f5831
nirajsahani166-byte <nirajsahani166@gmail.com>
2025-12-22 23:11:43 +0545

    Remove all tracked files

--- average.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a=3,b=4,c=5;
-    printf("average = %d",(a+b+c)/3);
-    return 0;
-}
--- test2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-int a,b,s;
-printf("enter the two numbers");
-scanf("%d%d",&a,&b);
-printf("the sum is %d",a+b);
-return 0;
-}

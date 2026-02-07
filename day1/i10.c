8fc4508ed42c33c6959d0e7c0817efbcd86546e8
nirajsahani166-byte <nirajsahani166@gmail.com>
2026-02-07 20:21:45 +0545

    deleted unwanted files

--- day1/i10.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-float a,b,c;
-printf("enter any three numbers");
-scanf("%f%f%f",&a,&b,&c);
-printf("average of three numbers =%f",(a+b+c)/3);
-return 0;
-}
--- day3/p4.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char name[20],add[30];
-    printf("enter your name and address");
-    scanf("%s%s",&name,&add);
-    printf("the name is %s and address is %s",name ,add);
-    return 0;
-}
